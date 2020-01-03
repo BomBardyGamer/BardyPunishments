a94b889302ea3833a451d14baf330f11335a6637
Callum Seabrook <yutubebroz@gmail.com>
2020-01-03 21:51:16 +0000

    minor fix to PlayerListeners class

--- src/main/java/xyz/bombardydevelopment/bardypunishments/listeners/PlayerListeners.java
+++ src/main/java/xyz/bombardydevelopment/bardypunishments/listeners/PlayerListeners.java
@@ -1,9 +1,13 @@
 package xyz.bombardydevelopment.bardypunishments.listeners;
 
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import xyz.bombardydevelopment.bardypunishments.BardyPunishments;
 import xyz.bombardydevelopment.bardypunishments.orm.PunishPlayer;
+import xyz.bombardydevelopment.bardypunishments.orm.Punishment;
+import xyz.bombardydevelopment.bardypunishments.util.PunishmentUtil;
 
 public class PlayerListeners implements Listener {
 
@@ -15,5 +19,11 @@ public class PlayerListeners implements Listener {
 
     public void handlePreLogin(AsyncPlayerPreLoginEvent event) {
         PunishPlayer player = plugin.getDatabase().getPlayerByUUID(event.getUniqueId()).join();
+        if (player == null) return;
+        if (!PunishmentUtil.hasActivePunishment(player, null, Punishment.PunishmentType.BAN)) return;
+        Punishment punishment = PunishmentUtil.getActivePunishment(player, null, Punishment.PunishmentType.BAN);
+        ComponentBuilder messageBuilder = new ComponentBuilder("");
+        event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_BANNED);
+        event.setKickMessage(TextComponent.toLegacyText(messageBuilder.create()));
     }
 }
